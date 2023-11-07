a09aa11c4406982a27e70bea9fab6954e4ad5f68
MCC676 <676teg@gmail.com>
2023-11-07 16:28:38 -0500

    cambios en index

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="public/css/login.css">
-    <title>Login de usuario111111111111111</title>
+    <title>Login de usuario1111111111324234234s11111</title>
   </head>
   <body>
   <div class="container-fluid ps-md-0">
