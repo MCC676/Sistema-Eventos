f6f2eecc045577bc27fdab1e365ee85c243eee57
MCC676 <676teg@gmail.com>
2023-11-07 18:32:01 -0500

    maquetado parte 2

--- modulos/header.php
+++ modulos/header.php
@@ -14,6 +14,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../public/css/menu.css">
+    <link rel="stylesheet" href="../public/librerias/fontawesome-free-6.4.2-web/css/all.css">
     <title>SIE </title>
   </head>
   <body style="background-color: #f8f9fa;">
